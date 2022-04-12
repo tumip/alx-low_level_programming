1ffb372e0e9cd2abf6dc2e553377bbd8a153c73c
Tumi <porotloanefb@gmail.com>
2022-04-11 20:50:20 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+for (n = 48; n <= 5; n++)
+{
+putchar(n);
+if (n == 57)
+{
+break;
+}
+
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
+
