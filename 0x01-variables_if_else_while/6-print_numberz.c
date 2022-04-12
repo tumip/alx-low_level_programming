c569597c3fc4ff944c854d5882c9fec30c74328a
Tumi <porotloanefb@gmail.com>
2022-04-11 20:15:10 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int bs;
+for (bs = 48; bs <= 57; bs++)
+{
+putchar(bs);
+}
+putchar('\n');
+return (0);
+}
