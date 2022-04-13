a0ac76602cf835d531cb62b60be137e26bd2106e
tumip <101490424+tumip@users.noreply.github.com>
2022-04-13 05:23:22 +0200

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,15 +9,22 @@
 */
 
 int print_last_digit(int n)
+
 {
-if (n == 0)
+int last_digit;
+
+if (n < 0)
 {
-return (0);
-}
-else 
-{
-return (n % 10);
+last_digit = (-1 * (n % 10));
+_putchar (last_digit + '0');
+return (last_digit);
 }
 
+else
+{
+last_digit = (n % 10);
+_putchar (last_digit + '0');
+return (last_digit);
+}
 
 }
