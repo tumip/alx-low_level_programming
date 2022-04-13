b4f3a840fca3d8dccfd96238ca79096191725b38
tumip <101490424+tumip@users.noreply.github.com>
2022-04-13 05:11:12 +0200

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*print_last_digit - function to print last digit
+*
+*@n: value to be returned
+*
+*Return: Always 0.
+*/
+
+int print_last_digit(int n)
+{
+for (n)
+{
+return (n % 10);
+}
+
+
+
+
+
+}
