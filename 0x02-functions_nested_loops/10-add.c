118089bf1828cffb341d533bbfd3b3fc04d812f8
tumip <101490424+tumip@users.noreply.github.com>
2022-04-13 05:40:52 +0200

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two values and returns the result
+ *@a: value added
+ *@b: value added
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
