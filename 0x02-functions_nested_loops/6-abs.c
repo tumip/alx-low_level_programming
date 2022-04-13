0e0114925d1da34c499523b4dca43b60d5febf92
tumip <101490424+tumip@users.noreply.github.com>
2022-04-13 05:02:38 +0200

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-*_abs - function for printing absolute value
-*
-*@n: input value
-*
-*Return: Always 0.
-*/
+ * _abs - value absolute
+ *
+ *@n: The int to print
+ * Return: Always 0.
+ */
+
 
 int _abs(int n)
 {
@@ -15,13 +16,15 @@ if (n < 0)
 {
 return (n * (-1));
 }
+
 else if (n == 0)
 {
 return (0);
 }
+
 else
 {
-return (0);
+return (n);
 }
 
 }
