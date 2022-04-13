c2194826924d6928ebf1c8243f7472d93910d73c
tumip <101490424+tumip@users.noreply.github.com>
2022-04-13 04:28:04 +0200

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,22 +7,25 @@
  */
 
 int print_sign(int n)
+
 {
 
 if (n > 0)
 {
-_putchar("+");
+_putchar('+');
 return (1);
 }
   
 else if (n < 0)
 {
-_putchar("-");
+_putchar('-');
 return (-1);
 }
 
 else
-_putchar("0");
+{
+_putchar('0');
 return (0);
+}
   
 }
