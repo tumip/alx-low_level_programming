f61b32b580140eacbd17f7ebf41bad8c8ae3465d
tumip <101490424+tumip@users.noreply.github.com>
2022-04-13 02:48:16 +0200

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
+
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-int _putchar(char c);
-=======
 int _putchar(char c); 
->>>>>>> 40dd483cc3e5e9f79fb6849e61faceff32f128b0
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -17,8 +14,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-<<<<<<< HEAD
 
 #endif /* _MAIN_H_ */
-=======
->>>>>>> 40dd483cc3e5e9f79fb6849e61faceff32f128b0
