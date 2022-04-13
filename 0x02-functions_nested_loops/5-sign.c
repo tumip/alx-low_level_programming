91a3c6d082e4596623d4ae02a155dc01f2432bbc
tumip <101490424+tumip@users.noreply.github.com>
2022-04-13 04:21:46 +0200

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - check your code
+ * 
+ * Return: Always 0.
+ */
+
+int print_sign(int n)
+{
+int n;
+if (n > 0)
+{
+_putchar(+);
+return (1);
+}
+  
+else if (n < 0)
+{
+_putchar(-);
+return (-1);
+}
+
+else
+_putchar(0);
+return (0);
+  
+}
