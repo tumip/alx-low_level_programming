4f0e70c0f79c96bd3d790a06340778eabd506b8c
tumip <101490424+tumip@users.noreply.github.com>
2022-04-13 02:45:58 +0200

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+  int a;
+  int redo;
+  
+  int = 0;
+  while (redo < 10)
+  {
+    for (a= 'a'; a <= 'z'; a++)
+    {
+      _putchar(a);
+    }
+   redo++;
+   _putchar('\n');
+  }
+  
+}
