0f6ef57fc272e9ad684a8130c7e57463dd18405e
tumip <101490424+tumip@users.noreply.github.com>
2022-04-15 05:38:11 +0200

    Task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ *main - largest prime facter of 612852475143.
+ *
+ *Return: 0
+ */
+int main(void)
+{
+long long int n, i, p_f;
+
+p_f = -1;
+n = 612852475143;
+
+while (x % 2 == 0)
+{
+p_f = 2;
+n = n / 2;
+}
+for (i = 3; i <= n/ 2; i = i + 2)
+	
+while (n % i == 0)
+{
+p_f = i;
+n =  n/ i;
+}
+}
+if (n > 2)
+p_f = n;
+p_f);
+return (0);
+}
