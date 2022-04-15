be3f4685f88d0483aa71106a0f41560bb5d8551e
tumip <101490424+tumip@users.noreply.github.com>
2022-04-15 04:48:15 +0200

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,22 +1,29 @@
 #include "main.h"
 
 /**
- * print_line - draws a straight line in the terminal
- * @n: number of times the character _ should be printed
+ * print_line - draws a straight line in the terminal.
+ *@n: print int
+ * Return: Always 0.
  */
+
 void print_line(int n)
 {
-if (n <= 0)
-{
-_putchar("\n");
-} else
-{
+
 int i;
-for (i = 1; i <= n; i++)
+
+if (n > 0)
 {
-_putchar("_");
+for (i = 0; i < n; i++)
+{
+_putchar (95);
 }
-_putchar("\n");
+
+_putchar ('\n');
+}
+
+else
+{
+_putchar ('\n');
 }
 
 }
