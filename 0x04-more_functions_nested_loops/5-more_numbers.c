e7a926b3a116d8c7d7cdf95c643fd37a2b41c94e
tumip <101490424+tumip@users.noreply.github.com>
2022-04-15 04:03:23 +0200

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ */
+
+void more_numbers(void)
+{
+int a, r;
+
+for (a = 0; a < 10; a++)
+{
+for (r = 0; r <= 14; r++)
+{
+if (r > 9)
+{
+_putchar((r / 10) + 48);
+}
+_putchar(( % 10) + 48);
+}
+_putchar(10);
+}
+}
