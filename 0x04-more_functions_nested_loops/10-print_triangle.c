2867378a3883b2da121dd8893e59a30951859db6
tumip <101490424+tumip@users.noreply.github.com>
2022-04-15 05:27:06 +0200

    Update Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,25 +7,25 @@
  */
 void print_triangle(int size)
 {
-	int i, j, ln;
+int i, j, ln;
 
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (i = 1; i <= size; i++)
-		{
-			for (ln = 1; ln <= (size - i); ln++)
-			{
-				_putchar(' ');
-			}
-			for (j = 1; j <= i; j++)
-			{
-				_putchar('#);
-			}
-			_putchar('\n')
-		}
-	}
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 1; i <= size; i++)
+{
+for (ln = 1; ln <= (size - i); ln++)
+{
+_putchar(' ');
+}
+for (j = 1; j <= i; j++)
+{
+_putchar('#);
+}
+_putchar('\n')
+}
+}
 }
