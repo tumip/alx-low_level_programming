8ddae1ded66a86c9b4cdb7321c94cf1ab3a29b2d
tumip <101490424+tumip@users.noreply.github.com>
2022-04-15 03:29:41 +0200

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,17 +2,10 @@
 
 /**
  * _isupper - checks for uppercase character
- * @c: the character to be tracked
+ * @letter: the character to be tracked
  * Return: 1 if c is uppercase, 0 otherwise
  */
-int _isupper(int c)
+int _isupper(int letter)
 {
-if (c >= 'A'; c <= 'Z')
-{
-return (1);
-{
-else
-{
-return (0);
-}
+	return (letter >= 'A' && letter <= 'Z');
 }
