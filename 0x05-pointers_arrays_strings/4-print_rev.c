2ff6b461e3b2be3083e203b336314384f5d853f8
tumip <101490424+tumip@users.noreply.github.com>
2022-04-21 02:50:25 +0200

    Task 4 2nd update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,26 +1,25 @@
 #include "main.h"
-
 /**
- * print_rev - prints str in rev
+ * print_rev - prints str
+ *@s: int pointer
  *
- * @s: int poiter to be changed
- *
- * Return: void
+ *Return: void 
  */
 
 void print_rev(char *s)
 {
 int i;
+
 i = 0;
-while (s[i] !='\0')
+while (s[i] != '\0')
 {
 i++;
 }
 
-for (i = i - 1; 1 >= 0; i--)
+for (i = i - 1 ; i >= 0; i--)
 {
 _putchar (s[i]);
 }
-_putchar('\n');
-}
 
+_putchar ('\n');
+}
