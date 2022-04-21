43d6cfb6eadbb2f4905987d37efb915b28fdb526
Tumi <porotloanefb@gmail.com>
2022-04-20 19:16:14 -0700

    Task 8 Update 2

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,14 +12,21 @@
 
 void print_array(int *a, int n)
 {
-int i;
-for (i = 0; i < n; i++)
+int ;
+
+i = 0;
+
+while (i < n)
 {
 printf("%d", a[i]);
-if (i != n -1)
+
+if (i < n - 1)
 {
 printf(", ");
 }
+
+i++;
+}
 printf("\n");
 }
-}
+
