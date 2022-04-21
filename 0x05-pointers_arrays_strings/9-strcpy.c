aa19b71df1d5c238de2f08a85846a59596cfa10c
Tumi <porotloanefb@gmail.com>
2022-04-20 19:28:29 -0700

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string pointed to by src
+ *
+ * @src: pointer
+ *
+ * @dest: pointer
+ *
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i, len;
+
+for (len = 0; src[len] !='\0'; len++)
+{
+}
+
+for (i = 0; i <= len; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
+
