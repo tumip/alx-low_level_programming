6c267d9f7ca6f45598d61fcb632e31fc52690bbd
tumip <101490424+tumip@users.noreply.github.com>
2022-04-21 05:14:54 +0200

    Task 11 Update 2

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,11 +10,28 @@
  */
 
 int main(void)
-{int num;
+{
+int ascii = 2772, i = 0, j, random;
+char password[100];
+time_t t;
 
-srand(time(0));
-num = rand();
-printf("%i\n", num);
+srand((int) time(&t));
+while (ascii > 126)
+{
+random = rand() % 126;
+password[i] = random;
+ascii -= random;
+i++;
+}
+if (ascii > 0)
+password[i] = ascii;
+else
+{
+i--;
+}
+for (j = 0; j <= i; j++)
+{
+printf("%c", password[j]);
+}
 return (0);
 }
-
