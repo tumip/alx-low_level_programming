6e6febff894147842d849ac6a8c9ec0ad0fffbd3
Tumi <porotloanefb@gmail.com>
2022-04-20 17:36:21 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts - prints str
+ *
+ * @str: str pointer
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+
+}
+
