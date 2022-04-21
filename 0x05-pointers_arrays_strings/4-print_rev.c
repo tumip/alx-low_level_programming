77e015988d9161ea0539283d87e1c96b2c8c624d
Tumi <porotloanefb@gmail.com>
2022-04-20 17:45:22 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints str in rev
+ *
+ * @s: int poiter to be changed
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+int i;
+i = 0;
+while (s[i] !='\0')
+{
+i++;
+}
+
+for (i = i - 1; 1 => 0; i--)
+{
+_putchar (s[i]);
+}
+_putchar('\n');
+}
+
