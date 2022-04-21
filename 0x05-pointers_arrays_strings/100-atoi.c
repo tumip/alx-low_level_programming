a4475938f24d4231a378f642c1c48801e647d60c
tumip <101490424+tumip@users.noreply.github.com>
2022-04-21 04:38:53 +0200

    Task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _atoi - print int 
+ * @s:  tested char
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	unsigned int counter, i, j, k, len, num, l;
+	int aux;
+
+	aux = 1;
+	counter = 0;
+	num = 0;
+
+	while (*(s + counter) != '\0')
+		counter++;
+	for (i = 0; i < counter; i++)
+	{
+		if (*(s + i) <= '9' && *(s + i) >= '0')
+		break;
+	}
+	for (j = i; j < counter; j++)
+	{
+		if (!(*(s + j) <= '9' && *(s + j) >= '0'))
+			break;
+	}
+
+	for (k = 0; k < i; k++)
+	{
+		if (*(s + k) == '-')
+			aux = -aux;
+	}
+	len = j - i;
+	l = i;
+		while (len >= 1)
+	{
+		num = num * 10 + (*(s + l) - '0');
+		l++;
+		len--;
+	}
+	return (num * aux);
+}
