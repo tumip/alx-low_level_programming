35dee145e788373dd0d21e6119b002c2dd1ff6b4
Tumi <porotloanefb@gmail.com>
2022-04-20 19:57:22 -0700

    Task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{int num;
+
+srand(time(0));
+num = rand();
+printf("%i\n", num);
+return (0);
+}
+
