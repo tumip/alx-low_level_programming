c7a964d98baffc691430072fe72cc2171916eb98
Tumi <porotloanefb@gmail.com>
2022-04-20 18:15:18 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other char of str
+ *
+ * @str: ref str
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+{
+_putchar(str[i]);
+}
+i++;
+}
+_putchar('\n');
+}
+
+
