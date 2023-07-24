aff4e5208fe41139f122a3055c66811030ec6386
Tumi <porotloanefb@gmail.com>
2023-07-24 04:31:48 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * A program that uses put to print
+ * Return 0: Success
+ */
+
+int main(void)
+	{
+		puts("Programming is like building a multilingual puzzle");
+		return (0);
+	}
+	
