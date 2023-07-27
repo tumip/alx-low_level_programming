8bbc5c228df56f2afdd7745403272225317b23b4
Tumi <porotloanefb@gmail.com>
2023-07-27 04:42:50 -0700

    Task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,13 +1,13 @@
- #include <stdio.h>
+#include <stdio.h>
 
- /**
- *main - Entry point
- *
- *program that prints all possible
- *combinations of single-digit numbers
- *
- *Return: Always 0 (Success)
- */
+/**
+*main - Entry point
+*
+*program that prints all possible
+*combinations of single-digit numbers
+*
+*Return: Always 0 (Success)
+*/
 
 int main(void)
 {
