03fda76c5b7197bcce79fca4e085e6d52ca6a90c
Tumi <porotloanefb@gmail.com>
2023-08-21 08:55:00 -0700

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *program that prints all possible
+ *different combinations of three digits
+ *
+ *Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+int i, j, k;
+for (i = 0; i <= 7; i++)
+	{
+	for (j = i + 1; j <= 8; j++)
+		{
+		for (k = j + 1; k <= 9; k++)
+			{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(k + '0');
+
+			if (i < 7)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
